66434946a3faebedec8650013955af8e3971495e
santafg <santanughosh28111998@gmail.com>
2021-04-28 13:08:13 +0530

    jj

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ const hbs = require('hbs');
 const cors = require('cors');
 const port = process.env.PORT || 8000;
 
-var corsOptions = {
-    origin: `http://api.openweathermap.org`,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const staticPath = path.join(__dirname , "../public");
 const templatePath = path.join(__dirname , "../templates/views");
@@ -28,13 +30,9 @@ app.get('' , (req , res) =>{
 app.get('/about' , (req , res) =>{
     res.render("about");
 })
-
-app.get('/weather', cors(corsOptions), function (req, res, next) {
+app.get('/weather' , (req , res) =>{
     res.render("weather")
-  })
-// app.get('/weather' , (req , res) =>{
-//     res.render("weather")
-// })
+})
 app.get('*' , (req , res) =>{
     res.render("error")
 })
